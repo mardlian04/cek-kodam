d068b4acbdf6432cc2d495c26fd5d8d2c1b37579
mardlian04 <163372024+mardlian04@users.noreply.github.com>
2024-06-23 00:54:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,13 +18,15 @@ document
 
 function calculateKodam(name) {
   const kodamList = [
-    "Sego Berkat",
-    "Gajah Baru",
-    "Seblak",
+    "Magic com sakti",
+    "Setan Merah tim papan tengah",
+    "The Nuruls",
     "Celeng",
     "Decul",
-    "Fans MU",
+    "Pinjam dulu seratus",
     "Ayam Geprek",
+    "Chat gpt",
+    "Prenjak alang alang",
   ];
   const nameValue = name.toLowerCase().charCodeAt(0) - 97;
   const kodamIndex = nameValue % kodamList.length;
